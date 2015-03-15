51364b00bffdff9305530de3d6f7af038cf89b7c
Ankitshah <ankit.tronix@gmail.com>
2015-03-15 19:51:20 +0530

    Enhance youtube-dl-functionality with aria2 using multi-threaded download. This also gives all functionality of youtube-dl

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# youtube-dl-with-aria
-Wrapper to download faster youtube-dl with aria to support multi-threaded-output
+Youtube-dl-with-aria
+===============
+A wrapper that speeds up youtube-dl downloads by utilising aria2's
+capability of opening multiple connections per server. This uses a lot of bandwidth so be careful.
+
+
+Dependencies
+============
+ * youtube-dl (http://rg3.github.io/youtube-dl/)
+ * aria2 (http://aria2.sourceforge.net/)
+Installation of both these utility is pretty standard. Google to find out the installation
+
+Usage
+=====
+bash youtube-dl-with-aria.sh <URL_of_download>
+
+
---
+++ youtube-dl-with-aria.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+UA=`youtube-dl --dump-user-agent`
+TMPDIR=`mktemp -d`
+COOKIES="$TMPDIR/cookies"
+trap "rm -rf $TMPDIR" 0
+ARIA_DNS_FLAGS=""
+aria2c -h#all|grep -- '--async-dns' >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+ARIA_DNS_FLAGS="--async-dns=false"
+fi
+# We need to separate the execution of youtube-dl and invocations of
+# aria2, since youtube-dl writes cookie data after it has performed
+# /all/ of its work.
+youtube-dl -o "[%(upload_date)s][%(id)s] %(title)s (by %(uploader)s).%(ext)s" --get-url --get-filename --cookies=${COOKIES} "$@" > ${TMPDIR}/video_data
+while read URL && read FILENAME
+do
+CLEANED_FILENAME=`echo "${FILENAME}" | tail -n 1 | tr ":\"" ";'" | tr -d "\\\/*?<>|"`
+echo "$CLEANED_FILENAME"
+aria2c $ARIA_DNS_FLAGS -c -j 3 -x 3 -s 3 -k 1M --load-cookies="$COOKIES" -U "$UA" -o "$CLEANED_FILENAME" "$URL"
+done < $TMPDIR/video_data
